ece64c9fbd885a923de7b1366db2d41f028cc4bf
Ilvif666 <ilvif666@gmail.com>
2019-02-12 00:39:10 +0300

    Mean, Variance & Standard Deviation computation from 5th homework

---
+++ statistics/statistics.py
@@ -0,0 +1,20 @@
+from math import sqrt
+#Please compute the sample mean, sample variance and standard deviation of the following time series:
+
+def sample_mean(list):
+	sum = 0
+	for i in list:
+		sum += i
+	return sum/len(list)
+
+def sample_variance(list):
+	sum = 0
+	for i in list:
+		sum += (i - sample_mean(list)) ** 2
+	return sum / (len(list)-1)
+
+def standard_deviation(list):
+	return sqrt(sample_variance(list))
+
+data = [-1, 2, 3, 1, 1]
+print(sample_mean(data),sample_variance(data),standard_deviation(data))
